8cc22ee251a2ee64fe32de776bbaa67b896ff235
Kaushal Kundu <k.kaushalkundu@gmail.com>
2024-10-08 05:53:51 +0530

    'GeneralInfo' completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,35 @@
 import { useState } from "react";
 import "./App.css";
+import GeneralInfo from "./Components/GeneralInfo";
+
+function manageDisplay(item) {
+  const computedStyle = window.getComputedStyle(item);
+  if (computedStyle.display === "none") {
+    item.style.display = "block";
+  } else {
+    item.style.display = "none";
+  }
+}
+
+function LabelInput(props) {
+  return (
+    <>
+      <label>
+        {props.text}: <input /> <br /> <br />
+      </label>
+    </>
+  );
+}
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="container">
+        <GeneralInfo />
+      </div>
+    </>
+  );
 }
 
 export default App;
+export { manageDisplay, LabelInput };
---
+++ src/Components/GeneralInfo.jsx
@@ -0,0 +1,28 @@
+import "../Styles/GeneralInfo.css";
+import { manageDisplay, LabelInput } from "../App";
+
+function GeneralInfo() {
+  return (
+    <>
+      <button
+        className="main1Btn"
+        onClick={() => {
+          const item = document.getElementById("generalInfoForm");
+          manageDisplay(item);
+        }}
+      >
+        General Information
+      </button>
+
+      <form id="generalInfoForm">
+        <LabelInput text="Name" />
+        <LabelInput text="Title" />
+        <LabelInput text="Phone No" />
+        <LabelInput text="Email" />
+        <LabelInput text="Address" />
+      </form>
+    </>
+  );
+}
+
+export default GeneralInfo;
